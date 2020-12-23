d9f734550b1d3158f4884c031d1ea2075329703f
Emine <keskincanemine@gmail.com>
2020-12-23 18:10:50 -0500

    add runner and feature file

---
+++ src/test/java/gmiBank_team22/step_Definitions/US_029_DataBase_Step_Def.java
@@ -0,0 +1,46 @@
+package gmiBank_team22.step_Definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class US_029_DataBase_Step_Def {
+    @Given("user  connecting GMI database")
+    public void user_connecting_GMI_database() {
+
+    }
+
+
+    @Given("user read all user data from database")
+    public void user_read_all_user_data_from_database() {
+
+    }
+
+    @Then("user validate all user data")
+    public void user_validate_all_user_data() {
+
+    }
+
+
+    @Given("user read all countries infos from database")
+    public void user_read_all_countries_infos_from_database() {
+
+    }
+
+    @Then("user validate all countries infos")
+    public void user_validate_all_countries_infos() {
+
+    }
+
+
+    @Given("user read all states of related to USA from database")
+    public void user_read_all_states_of_related_to_USA_from_database() {
+
+    }
+
+    @Then("user")
+    public void user() {
+
+    }
+
+
+}
