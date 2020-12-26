6d1e57887fd79e25be15c652c5e96d0358fe54de
VENUS27 <aliuzung@gmail.com>
2020-12-25 20:37:10 -0500

    friday update from mali

--- src/test/java/gmiBank_team22/pojos/States.java
+++ src/test/java/gmiBank_team22/pojos/States.java
@@ -2,34 +2,82 @@ package gmiBank_team22.pojos;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class States {
-    private int id;
-    private String name;
-    private Country country;
 
-    public int getId() {
+    private Integer id;
+
+    private String name;
+
+    private Object tpcountry;
+
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+
+    public Integer getId() {
         return id;
     }
-    public void setId(int id) {
+
+
+    public void setId(Integer id) {
         this.id = id;
     }
+
 
     public String getName() {
         return name;
     }
+
+
     public void setName(String name) {
         this.name = name;
     }
 
 
-    public Country getCountry() {
-        return country;
-    }
-    public void setCountry(Country country) {
-        this.country = country;
+    public Object getTpcountry() {
+        return tpcountry;
     }
 
+
+    public void setTpcountry(Object tpcountry) {
+        this.tpcountry = tpcountry;
+    }
+
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+    public States() {
+    }
+    public States(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    public States(String name, Object tpcountry) {
+        this.name = name;
+        this.tpcountry = tpcountry;
+    }
+
+    public States(Integer id, String name, Object tpcountry) {
+        this.id = id;
+        this.name = name;
+        this.tpcountry = tpcountry;
+    }
+
+
+    public String toString() {
+        return new ToStringBuilder(this).append("id", id).append("name", name).append("tpcountry", tpcountry).append("additionalProperties", additionalProperties).toString();
+    }
 
 }
